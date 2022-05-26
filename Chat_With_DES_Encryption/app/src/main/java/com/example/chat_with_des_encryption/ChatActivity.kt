a9a23960b4727694da7fb4b523bd7ea6f91b5667
maxbarros <mbds.snf19@uea.edu.br>
2022-05-26 04:44:02 -0400

    COMMIT progresso 02

---
+++ Chat_With_DES_Encryption/app/src/main/java/com/example/chat_with_des_encryption/ChatActivity.kt
@@ -0,0 +1,11 @@
+package com.example.chat_with_des_encryption
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
